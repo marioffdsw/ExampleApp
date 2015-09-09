dbe7e19383d6c4cdc36a77a86db26303e5b115a3
Mario <marioffdsw@outlook.com>
2015-09-09 00:30:19 -0500

    Listing 10-2. The Contents of the Repository.cs File

---
+++ ExampleApp/Models/Repository.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace ExampleApp.Models
+{
+    public class Repository
+    {
+        private Dictionary<int, Product> data;
+        private static Repository repo;
+
+        static Repository()
+        {
+            repo = new Repository();
+        }
+
+        public static Repository Current
+        {
+            get { return repo; }
+        }
+
+        public Repository()
+        {
+            Product[] products = new Product[]
+            {
+                new Product { ProductId = 1, Name = "Kayak", Price = 275M },
+                new Product { ProductId = 2, Name = "Lifejacket", Price = 48.95M },
+                new Product { ProductId = 3, Name = "Soccer Ball", Price = 19.50M },
+                new Product { ProductId = 4, Name = "Thinking Cap", Price = 16M },
+            };
+
+            data = new Dictionary<int, Product>();
+
+            foreach (Product prod in products)
+            {
+                data.Add( prod.ProductId, prod );
+            }
+        }
+
+        public IEnumerable<Product> Products
+        {
+            get { return data.Values; }
+        }
+
+        public Product GetProduct( int id )
+        {
+            return data[id];
+        }
+
+        public Product SaveProduct( Product newProduct )
+        {
+            newProduct.ProductId = data.Keys.Count + 1;
+            return data[newProduct.ProductId] = newProduct;
+        }
+
+        public Product DeleteProduct( int id )
+        {
+            Product prod = data[ id ];
+
+            if (prod != null)
+            {
+                data.Remove( id );
+            }
+
+            return prod;
+        }
+    }
+}
