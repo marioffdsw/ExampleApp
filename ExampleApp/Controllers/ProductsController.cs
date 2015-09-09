99536267132d1d10febe164a44ba00d48c2b4b3d
Mario <marioffdsw@outlook.com>
2015-09-09 00:33:42 -0500

    Listing 10-3. The Contents of the ProductsController.cs File

---
+++ ExampleApp/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using ExampleApp.Models;
+
+namespace ExampleApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Repository repo;
+
+        public ProductsController()
+        {
+            repo = Repository.Current;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return repo.Products;
+        }
+    }
+}
