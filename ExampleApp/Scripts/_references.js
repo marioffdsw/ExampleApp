cf8e460dce575a7dea3ec54b15e64f2f15b46d5a
Mario <marioffdsw@outlook.com>
2015-09-09 00:43:29 -0500

    Listing 10-6. The Contents of the exampleApp.js File

---
+++ ExampleApp/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="exampleapp.js" />
+/// <reference path="jquery-1.10.2.js" />
+/// <reference path="jquery-2.1.0.js" />
+/// <reference path="knockout-3.1.0.js" />
+/// <reference path="modernizr-2.6.2.js" />
