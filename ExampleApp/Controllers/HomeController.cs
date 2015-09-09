384d6205895659c92ac9b362256237cd0179720d
Mario <marioffdsw@outlook.com>
2015-09-09 00:40:07 -0500

    Listing 10-5. The Contents of the Index.cshtml File

---
+++ ExampleApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using ExampleApp.Models;
+
+namespace ExampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        Repository repo;
+
+        public HomeController()
+        {
+            repo = Repository.Current;
+        }
+
+        public ActionResult Index()
+        {
+            return View( repo.Products );
+        }
+    }
+}
